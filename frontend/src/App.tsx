839b0097c39ce6f72a0b7ce7256fc48cc6434b65
Ramiro Rosales <ramirosales52@gmail.com>
2025-09-13 14:14:25 -0300

    feat: header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import ImcCalculator from '@/components/ImcCalculator'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <div className='py-4 min-h-screen w-screen flex flex-col justify-center items-center bg-slate-50'>
+      <div className='absolute top-0 w-full'>
+        <Header />
+      </div>
       <ImcCalculator />
     </div>
   )
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,19 @@
+import Login from "./Login"
+import Register from "./Register"
+import UtnLogo from "@/assets/utn-logo.png"
+
+const Header = () => {
+  return (
+    <div className="bg-slate-200 px-4">
+      <div className="flex justify-between items-center">
+        <img src={UtnLogo} className="h-16" />
+        <div className="flex gap-3">
+          <Login />
+          <Register />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
