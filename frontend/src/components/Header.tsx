a11e3b8575e80d70e758432d27391cd1e617c8e8
Ramiro Rosales <ramirosales52@gmail.com>
2025-09-13 14:20:01 -0300

    fix: alt en logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className="bg-slate-200 px-4">
       <div className="flex justify-between items-center">
-        <img src={UtnLogo} className="h-16" />
+        <img src={UtnLogo} alt="UTN Logo" className="h-16" />
         <div className="flex gap-3">
           <Login />
           <Register />
