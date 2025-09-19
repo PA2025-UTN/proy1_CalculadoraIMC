688975b022353ee12ed5160490836a996d638a34
Ramiro Rosales <ramirosales52@gmail.com>
2025-09-19 11:18:08 -0300

    feat: spinner

--- frontend/src/utils/PublicRoute.tsx
+++ frontend/src/utils/PublicRoute.tsx
@@ -1,13 +1,13 @@
 import { Navigate, Outlet } from "react-router"
 import { useIsAuth } from "@/components/auth/hooks/useIsAuth"
+import Spinner from "@/components/ui/spinner"
 
 const PublicRoute = () => {
   const isAuth = useIsAuth()
 
-  if (isAuth === null) return <div className="h-screen w-full p-4">Cargando...</div>
+  if (isAuth === null) return <div className="h-screen w-full flex items-center justify-center"><Spinner /></div>
 
   return isAuth ? <Navigate to="/calculadora" /> : <Outlet />
 }
 
 export default PublicRoute
-
