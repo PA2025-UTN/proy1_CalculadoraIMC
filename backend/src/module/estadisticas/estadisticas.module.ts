d5714fc16a7d51287e751d75147a16a08fa2df6f
Ramiro Rosales <ramirosales52@gmail.com>
2025-09-26 17:38:58 -0300

    fix: error de dependencias en el modulo de estadisticas

--- backend/src/module/estadisticas/estadisticas.module.ts
+++ backend/src/module/estadisticas/estadisticas.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EstadisticasService } from './estadisticas.service';
 import { EstadisticasController } from './estadisticas.controller';
+import { EstadisticasRepository } from './repositories/estadisticas.repository';
+import { Imc } from '../imc/entities/imc.entity';
+import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Imc]),
+    AuthModule,
+    UsersModule
+  ],
   controllers: [EstadisticasController],
-  providers: [EstadisticasService],
+  providers: [EstadisticasService, EstadisticasRepository],
 })
 export class EstadisticasModule {}
